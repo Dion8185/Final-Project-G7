6cd3c78b22261407d480ae987fc555fc8320a483
Dion8185 <justindion180@gmail.com>
2026-03-27 21:53:10 +0800

    Refactor teacher registration page and enhance validation
    - Improved HTML structure and formatting in register_teacher.html for better readability.
    - Updated validation logic for name fields, email, and password to enhance user input checks.
    - Added feedback messages for form validation to improve user experience.
    - Integrated Flask-Mail configuration in __init__.py for email functionalities.
    - Removed unnecessary compiled Python files from __pycache__ directories.

--- testpoint/__init__.py
+++ testpoint/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_mail import Mail
 
 db_config = {
     'host': 'localhost',
@@ -8,10 +9,23 @@ db_config = {
     'auth_plugin': 'mysql_native_password'
 }
 
+mail = Mail()
+
 def create_app():
 
     app = Flask(__name__)
     app.secret_key = "Secret@123_key"
+    
+    app = Flask(__name__)
+    app.secret_key = "Dion.Secret@Key"
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'lucator51plus1@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'yzfc xmrb bkpw yepp'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+
+    mail.init_app(app)
     
     from testpoint.Auth.login import auth
     from testpoint.Admin.admin import admin
